534e45be42e3391c81c1e6577b6c751aed6aac9d
Iurii <yuri.ivanoff@gmail.com>
2025-12-10 13:12:10 +0300

    Minor fixes in the notes/api.py file.

--- apis/notes/api.py
+++ apis/notes/api.py
@@ -1,7 +1,7 @@
 import allure
 import httpx
 from config.headers import Headers
-from utils.helper import Helper
+from utils.tools import Helper
 
 from apis.notes.endpoints import Endpoints
 from apis.notes.models.model import NotesModel
@@ -22,7 +22,7 @@ class Notes(Helper):
             headers=self.headers.auth,
             json=self.payloads.create_note,
         )
-        assert response.status_code == 200, f"Something goes wrong!\n {response.json()}"  # noqa: S101
+        assert response.status_code == 200, f"Something goes wrong!\n {response.json()}"
         self.attach_response(response.json())
         model = NotesModel(**response.json())
         return model
@@ -30,7 +30,7 @@ class Notes(Helper):
     @allure.step("Get note by ID")
     def get_note_by_id(self, id: str) -> NotesModel:
         response = httpx.get(url=self.endpoints.get_note_by_id(id), headers=self.headers.auth)
-        assert response.status_code == 200, f"Something goes wrong!\n {response.json()}"  # noqa: S101
+        assert response.status_code == 200, f"Something goes wrong!\n {response.json()}"
         self.attach_response(response.json())
         model = NotesModel(**response.json())
         return model
