ef14d479f8401b5148eebb8c19d5fee46c473f58
Iurii <yuri.ivanoff@gmail.com>
2025-12-12 14:26:25 +0300

    Add steps Get a note by ID (note not found and Get random note ID from existing notes to the api_client.py.

--- apis/notes/api_client.py
+++ apis/notes/api_client.py
@@ -1,10 +1,17 @@
+import random
+
 import allure
 import httpx
 from config.headers import Headers
 from utils.helper import Helper
 
 from apis.notes.endpoints import Endpoints
-from apis.notes.models import NoteDeletedModel, NoteModel, NotesModel
+from apis.notes.models import (
+    DeleteNoteSuccessfulRequestModel,
+    NoteModel,
+    NoteNotFoundModel,
+    NotesModel,
+)
 from apis.notes.payloads import Payloads
 
 
@@ -43,6 +50,14 @@ class Notes(Helper):
         model = NoteModel(**response.json())
         return model
 
+    @allure.step("Get a note by ID (note not found)")
+    def get_note_by_id__note_not_found(self, id: str) -> NoteNotFoundModel:
+        response = httpx.get(url=self.endpoints.get_note_by_id(id), headers=self.headers.auth)
+        assert response.status_code == 404, f"Something goes wrong!\n {response.json()}"
+        self.attach_response(response.json())
+        model = NoteNotFoundModel(**response.json())
+        return model
+
     @allure.step("Update an existing note")
     def update_existing_note(self, id: str) -> NoteModel:
         response = httpx.put(
@@ -56,9 +71,19 @@ class Notes(Helper):
         return model
 
     @allure.step("Delete a note by ID")
-    def delete_note_by_id(self, id: str) -> NoteDeletedModel:
+    def delete_note_by_id(self, id: str) -> DeleteNoteSuccessfulRequestModel:
         response = httpx.delete(url=self.endpoints.delete_note_by_id(id), headers=self.headers.auth)
         assert response.status_code == 200, f"Something goes wrong!\n {response.json()}"
         self.attach_response(response.json())
-        model = NoteDeletedModel(**response.json())
+        model = DeleteNoteSuccessfulRequestModel(**response.json())
         return model
+
+    @allure.step("Get random note ID from existing notes")
+    def get_random_note_id_from_existing(self) -> str:
+        notes = self.get_all_notes().model_dump()
+        assert notes, f"Current user doesn't have any note at the moment!\n{notes}"
+        ids = []
+        for item in notes["data"]:
+            ids.append(item["id"])
+
+        return str(random.choice(ids))
