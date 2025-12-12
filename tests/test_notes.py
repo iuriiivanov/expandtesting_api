943488de38baf94be6fc8e2fe5ce19f1b295ba89
Iurii <yuri.ivanoff@gmail.com>
2025-12-12 14:25:06 +0300

    Add test Delete a note by ID to the test_notes.py.

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,5 +1,6 @@
 import allure
 import pytest
+import random
 
 from config.base_test import BaseTest
 
@@ -19,3 +20,10 @@ class TestNotes(BaseTest):
     @allure.title("Get all notes")
     def test_get_all_notes(self) -> None:
         self.api_notes.get_all_notes()
+
+    @pytest.mark.regression
+    @allure.title("Delete a note by ID")
+    def test_delete_note_by_id(self) -> None:
+        id_to_delete = self.api_notes.get_random_note_id_from_existing()
+        self.api_notes.delete_note_by_id(id_to_delete)
+        self.api_notes.get_note_by_id__note_not_found(id_to_delete)
