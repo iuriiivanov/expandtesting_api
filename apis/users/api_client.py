e2f9318efe422961bc18bd39fdc4bcf07b01da27
Iurii <yuri.ivanoff@gmail.com>
2025-12-23 21:23:14 +0300

    Remove api_client.py (users).

--- apis/users/api_client.py
+++
@@ -1,8 +0,0 @@
-from apis.users.payloads import Payloads
-from utils.helper import Helper
-
-
-class Users(Helper):
-    def __init__(self):
-        super().__init__()
-        self.payload = Payloads()
