2a2fc3c8fc61c74e18fa3ade6d18829ade76f474
Iurii <yuri.ivanoff@gmail.com>
2025-12-25 19:25:48 +0300

    Add parametrization for test_create_note.

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -3,6 +3,7 @@ import pytest
 import random
 
 from config.base_test import BaseTest
+from apis.notes.payloads import Payloads
 
 
 @allure.epic("Notes manipulation")
@@ -12,8 +13,25 @@ class TestNotes(BaseTest):
 
     @pytest.mark.regression
     @allure.title("Create a new note")
-    def test_create_note(self) -> None:
-        note = self.api_notes.create_note()
+    @pytest.mark.parametrize(
+        "payload",
+        [
+            Payloads.create_note,
+            Payloads.create_note_min_title,
+            Payloads.create_note_min_description,
+            Payloads.create_note_max_title,
+            Payloads.create_note_max_description,
+        ],
+        ids=[
+            "Create a note with default options",
+            "Create a note with the shortest title length",
+            "Create a note with the shortest description length",
+            "Create a note with the longest title length",
+            "Create a note with the longest description length",
+        ],
+    )
+    def test_create_note(self, payload: dict[str, str]) -> None:
+        note = self.api_notes.create_note(payload)
         self.api_notes.get_note_by_id(note.data.id)
 
     @pytest.mark.regression
@@ -24,7 +42,7 @@ class TestNotes(BaseTest):
     @pytest.mark.regression
     @allure.title("Get a note by ID")
     def test_get_note_by_id(self) -> None:
-        note_created = self.api_notes.create_note()
+        note_created = self.api_notes.create_note(Payloads.create_note)
         note_recieved = self.api_notes.get_note_by_id(note_created.data.id)
         self.api_notes.verify_note_data(note_created.data, note_recieved.data)
 
