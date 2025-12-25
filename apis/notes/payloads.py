298e863906ac4d62a886530ceec362d3b939035e
Iurii <yuri.ivanoff@gmail.com>
2025-12-25 17:24:17 +0300

    Notes Payloads refactoring. Remove ClassVar. Use __init__. Add a few methods for good note.

--- apis/notes/payloads.py
+++ apis/notes/payloads.py
@@ -1,5 +1,4 @@
 import random
-from typing import ClassVar
 
 from faker import Faker
 
@@ -9,19 +8,37 @@ faker = Faker()
 class Payloads:
     """Returns payloads for note APIs."""
 
-    create_note: ClassVar[dict[str, str]] = {
-        "title": faker.sentence(nb_words=5, variable_nb_words=True),
-        "description": faker.text(max_nb_chars=50),
-        "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
-    }
-
-    update_existing_note: ClassVar[dict[str, str | bool]] = {
-        "title": faker.sentence(nb_words=5, variable_nb_words=True),
-        "description": faker.text(max_nb_chars=50),
-        "completed": random.choice([True, False]),  # noqa: S311
-        "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
-    }
-
-    change_completed_status_to_true: ClassVar[dict[str, str | bool]] = {"completed": True}
-
-    change_completed_status_to_false: ClassVar[dict[str, str | bool]] = {"completed": False}
+    def __init__(self) -> None:
+        self.create_note: dict[str, str] = {
+            "title": faker.text(max_nb_chars=100),
+            "description": faker.text(max_nb_chars=1000),
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.create_note_min_title: dict[str, str] = {
+            "title": "TITL",
+            "description": faker.text(max_nb_chars=1000),
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.create_note_min_description: dict[str, str] = {
+            "title": faker.text(max_nb_chars=100),
+            "description": "DESC",
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.create_note_max_title: dict[str, str] = {
+            "title": "Title 100 Title 100 Title 100 Title 100 Title 100 Title 100 Title 100 Title 100 Title 100 Title 100!",
+            "description": faker.text(max_nb_chars=1000),
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.create_note_max_description: dict[str, str] = {
+            "title": faker.text(max_nb_chars=100),
+            "description": "Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description 1000 Description!!!",
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.update_existing_note: dict[str, str | bool] = {
+            "title": faker.sentence(nb_words=5, variable_nb_words=True),
+            "description": faker.text(max_nb_chars=50),
+            "completed": random.choice([True, False]),  # noqa: S311
+            "category": random.choice(["Home", "Work", "Personal"]),  # noqa: S311
+        }
+        self.change_completed_status_to_true: dict[str, str | bool] = {"completed": True}
+        self.change_completed_status_to_false: dict[str, str | bool] = {"completed": False}
