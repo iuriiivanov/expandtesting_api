47d38f698e64bea4a2b2594c92d6951ecad57eec
Iurii <yuri.ivanoff@gmail.com>
2025-12-25 19:27:12 +0300

    Small fix for test_notes.py.

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -55,7 +55,7 @@ class TestNotes(BaseTest):
     @pytest.mark.regression
     @allure.title("Update an existing note")
     def test_update_note(self) -> None:
-        note = self.api_notes.create_note()
+        note = self.api_notes.create_note(Payloads.create_note)
         note_updated = self.api_notes.update_note(note.data.id)
         note_received = self.api_notes.get_note_by_id(note.data.id)
         self.api_notes.verify_note_data(note_updated.data, note_received.data)
@@ -63,7 +63,7 @@ class TestNotes(BaseTest):
     @pytest.mark.regression
     @allure.title("Update the completed status of the note")
     def test_update_completed_status_of_note(self) -> None:
-        note = self.api_notes.create_note()
+        note = self.api_notes.create_note(Payloads.create_note)
         self.api_notes.change_completed_status_to_true(note.data.id)
         self.api_notes.change_completed_status_to_false(note.data.id)
 
