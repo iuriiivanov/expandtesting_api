1275e2337a49138e795cfe7c0b433e1d7e48abc6
iuriiivanov <yuri.ivanoff@gmail.com>
2026-03-23 22:21:45 +0300

    Change api/notes/__init__.py.

--- apis/notes/__init__.py
+++ apis/notes/__init__.py
@@ -1,3 +1,17 @@
 """
 Notes APIs Package
 """
+
+from .endpoints import Endpoints
+from .models import DeleteNote200Model, Note404Model, NoteDataModel, NoteModel, NotesModel
+from .payloads import Payloads
+
+__all__ = [
+    "DeleteNote200Model",
+    "Endpoints",
+    "Note404Model",
+    "NoteDataModel",
+    "NoteModel",
+    "NotesModel",
+    "Payloads",
+]
