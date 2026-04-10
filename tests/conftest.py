17bcb1e082813196f44e1ca689cc862785b34348
iuriiivanov <yuri.ivanoff@gmail.com>
2026-04-10 18:22:34 +0300

    Change conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-import httpx
 import pytest
+import requests
 
 API_BASE_URL = "https://practice.expandtesting.com/notes/api"
 
 
 @pytest.fixture(autouse=True, scope="session")
 def health_check() -> None:
-    response = httpx.get(url=f"{API_BASE_URL}/health-check")
+    response = requests.get(url=f"{API_BASE_URL}/health-check")
     assert response.status_code == 200
